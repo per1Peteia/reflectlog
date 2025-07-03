2a1742166c6aca7e957a11ae8c3ede47cb82e00c
per1Peteia <justusmanthey@googlemail.com>
2025-07-03 16:00:10 +0200

    feat: command clip pbpastes clipboard content, makes LLM call for a subject title

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/anthropics/anthropic-sdk-go"
 	"github.com/mitchellh/cli"
@@ -22,11 +25,10 @@ func (c *defaultCommand) Help() string {
 }
 
 func (c *defaultCommand) Synopsis() string {
-	return "default behaviour"
+	return "open a TUI to browse different materials"
 }
 
 func (c *defaultCommand) Run(args []string) int {
-	fmt.Println(rflHelp)
 	return 0
 }
 
@@ -67,6 +69,53 @@ func (c *chatCommand) Run(args []string) int {
 	return 0
 }
 
+func clipCommandFactory() (cli.Command, error) {
+	return &clipCommand{}, nil
+}
+
+type clipCommand struct{}
+
+func (c *clipCommand) Help() string {
+	return ""
+}
+
+func (c *clipCommand) Synopsis() string {
+	return "clip some information you want to capture, saves it to the db to makes it processable either programmatically or for the agent"
+}
+
+func (c *clipCommand) Run(args []string) int {
+	out, err := exec.Command("pbpaste").Output()
+	if err != nil {
+		return 1
+	}
+
+	cbContent := strings.TrimSpace(string(out))
+	fmt.Println(cbContent)
+
+	client := anthropic.NewClient()
+	message := anthropic.NewUserMessage(anthropic.NewTextBlock(cbContent))
+	synopsis, err := client.Messages.New(context.TODO(), anthropic.MessageNewParams{
+		System:    []anthropic.TextBlockParam{{Text: clipSynopsisPrompt}},
+		Model:     anthropic.ModelClaude4Sonnet20250514,
+		Messages:  []anthropic.MessageParam{message},
+		MaxTokens: int64(1024),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+
+	for _, content := range synopsis.Content {
+		switch content.Type {
+		case "text":
+			fmt.Println(content.Text)
+		}
+	}
+
+	return 0
+}
+
 func InitCLI() int {
 	c := cli.NewCLI("app", "1.0.0")
 	c.Args = os.Args[1:]
@@ -75,6 +124,7 @@ func InitCLI() int {
 		// "init": initCommandFactory,
 		"chat": chatCommandFactory,
 		// "log":  logCommandFactory,
+		"clip": clipCommandFactory,
 	}
 
 	exitStatus, err := c.Run()
---
+++ internal/cli/prompts.go
@@ -0,0 +1,13 @@
+package cli
+
+import ()
+
+const clipSynopsisPrompt = `- You are a seasoned tutor for students. 
+- you will get sent pieces of information and will write a subject line for it. 
+- your students want want something that identifies/labels the topic without condensing the information - more like a catalog entry or index heading that tells you "this is the document about X" rather than "here's what X says."
+- always use 140 characters or less.
+- your students can save this information and quickly recall what was notable about the saved information from a learning standpoint 
+- The subject line will also be functioning as a logging statement, to recall what was saved
+- dont use markdown
+- dont start the line with things like "subject:" etc.
+`
